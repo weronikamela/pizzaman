22c6bd65fb9fb3350dfbef0de68f012a7aba289d
igor.cukovic <igor.cukovic@accenture.com>
2017-02-17 14:49:30 +0100

    Added new model and improved templates with newly created data

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+class Product(models.Model):
+    name        = models.CharField(max_length=32)
+    description = models.CharField(max_length=255)
+    price       = models.IntegerField()
+    currency    = models.CharField(max_length=32)
+    picture     = models.CharField(max_length=32)
