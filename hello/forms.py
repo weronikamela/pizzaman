4afbb5342a267a38f2317d5fd42613dbe22a4e7d
daniel.stanczak <daniel.stanczak@accenture.com>
2017-02-26 19:46:44 +0100

    Removed unused url, added labels to the form and improved css;

--- hello/forms.py
+++ hello/forms.py
@@ -4,6 +4,7 @@ from django.core import validators
 class OrderForm(forms.Form):
     customerName = forms.CharField(
         max_length=32,
+        label ="Customer's name",
         widget=forms.TextInput(
             attrs={
                 'class': 'input-button-medium',
@@ -13,6 +14,7 @@ class OrderForm(forms.Form):
     )
     customerSurname = forms.CharField(
         max_length=32,
+        label ="Customer's surname",
         widget=forms.TextInput(
             attrs={
                 'class': 'input-button-medium',
@@ -22,6 +24,7 @@ class OrderForm(forms.Form):
     )
     customerStreet = forms.CharField(
         max_length=32,
+        label ="Customer's street and number address",
         widget=forms.TextInput(
             attrs={
                 'class': 'input-button-medium',
@@ -31,6 +34,7 @@ class OrderForm(forms.Form):
     )
     customerPostalCode = forms.CharField(
         max_length=32,
+        label ="Customer's postal code",
         widget=forms.TextInput(
             attrs={
                 'class': 'input-button-medium',
@@ -40,6 +44,7 @@ class OrderForm(forms.Form):
     )
     customerCity = forms.CharField(
         max_length=32,
+        label ="Customer's city",
         widget=forms.TextInput(
             attrs={
                 'class': 'input-button-medium',
@@ -49,6 +54,7 @@ class OrderForm(forms.Form):
     )
     customerEmail = forms.EmailField(
         max_length=32,
+        label ="Customer's email",
         validators=[validators.validate_email],
         widget=forms.TextInput(
             attrs={
@@ -59,6 +65,7 @@ class OrderForm(forms.Form):
     )
     customerPhone = forms.CharField(
         max_length=32,
+        label ="Customer's phone number",
         widget=forms.TextInput(
             attrs={
                 'class': 'input-button-medium',
